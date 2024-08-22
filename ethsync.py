8fa720a5615b532024fb1bf252fa07a4d030babe
Tomas <1979739+maxysoft@users.noreply.github.com>
2024-08-22 16:13:15 +0200

    Update ethsync.py
    - Updated dependences to make it work with python 3.7.
    - Added websocket timeout and error handling. Rised websocket payload size to avoid "websockets.exceptions.PayloadTooBig: over size limit" with blocks that have a lot of transactions.
    - Updated database connection.
    - Updated contributors.

--- ethsync.py
+++ ethsync.py
@@ -2,35 +2,39 @@
 # https://github.com/Adamant-im/ETH-transactions-storage
 
 # Contributors:
-# v2.4.1
+# v2.4.2
+# 2024- @maxysoft (maxysoft@maxynetwork.it)
 # 2022-2024 ADAMANT Foundation (devs@adamant.im), @twhitehead00, Tyvan Cheng (tyvancheng@gmail.com)
 # 2021-2022 ADAMANT Foundation (devs@adamant.im), Francesco Bonanno (mibofra@parrotsec.org),
 # Guénolé de Cadoudal (guenoledc@yahoo.fr), Drew Wells (drew.wells00@gmail.com)
 # 2020-2021 ADAMANT Foundation (devs@adamant.im): Aleksei Lebedev
 # 2017-2020 ADAMANT TECH LABS LP (pr@adamant.im): Artem Brunov, Aleksei Lebedev
 
-from os import environ
+import os
 from web3 import Web3
+from web3.exceptions import TimeExhausted
 from web3.middleware import geth_poa_middleware
 import psycopg2
 import time
 import sys
 import logging
-#from systemd.journal import JournalHandler
 
 # Get env variables or set to default
-dbname = environ.get("DB_NAME")
-startBlock = environ.get("START_BLOCK") or "1"
-confirmationBlocks = environ.get("CONFIRMATIONS_BLOCK") or "0"
-nodeUrl = environ.get("ETH_URL")
-pollingPeriod = environ.get("PERIOD") or "20"
-logFile = environ.get("LOG_FILE")
+dbuser = os.getenv("DB_USER")
+dbpassword = os.getenv("DB_PASSWORD")
+databasename = os.getenv("DATABASE_NAME")
+dbhost = os.getenv("DB_HOST")
+startBlock = os.getenv("START_BLOCK") or "1"
+confirmationBlocks = os.getenv("CONFIRMATIONS_BLOCK") or "0"
+nodeUrl = os.getenv("ETH_URL")
+pollingPeriod = os.getenv("PERIOD") or "20"
+logFile = os.getenv("LOG_FILE")
 
-if dbname == None:
-    print('Add postgre database in env var DB_NAME')
+if databasename is None:
+    print('Add postgre database in env var DATABASE_NAME')
     exit(2)
 
-if nodeUrl == None:
+if nodeUrl is None:
     print('Add eth url in env var ETH_URL')
     exit(2)
 
@@ -38,19 +42,18 @@ if nodeUrl == None:
 if nodeUrl.startswith("http"):
     web3 = Web3(Web3.HTTPProvider(nodeUrl)) # "http://publicnode:8545"
 elif nodeUrl.startswith("ws"):
-    web3 = Web3(Web3.WebsocketProvider(nodeUrl)) # "ws://publicnode:8546"
+    web3 = Web3(Web3.WebsocketProvider(nodeUrl, websocket_timeout=60, websocket_kwargs={'max_size': 10**7})) # "ws://publicnode:8546"
 else:
     web3 = Web3(Web3.IPCProvider(nodeUrl)) # "/home/geth/.ethereum/geth.ipc"
 
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # Start logger
-#logger = logging.getLogger("EthIndexerLog")
 logger = logging.getLogger("eth-sync")
 logger.setLevel(logging.INFO)
 
 # File logger
-if logFile == None:
+if logFile is None:
     lfh = logging.StreamHandler()
 else:
     lfh = logging.FileHandler(logFile)
@@ -67,12 +70,13 @@ logger.addHandler(lfh)
 #logger.addHandler(ljc)
 
 try:
-    logger.info("Trying to connect to " + dbname + " database…")
-    conn = psycopg2.connect(database=dbname)
+    logger.info("Trying to connect to " + databasename + " database…")
+    conn_string = f"dbname={databasename} user={dbuser} password={dbpassword} host={dbhost}"
+    conn = psycopg2.connect(conn_string)
     conn.autocommit = True
     logger.info("Connected to the database")
-except:
-    logger.error("Unable to connect to database")
+except Exception as e:
+    logger.error(f"Unable to connect to database: {e}")
     exit(1)
 
 # Delete last block as it may be not imported in full
@@ -126,18 +130,29 @@ def insertTxsFromBlock(block):
             (time, fr, to, value, gas, gasprice, blockid, txhash, contract_to, contract_value))
 
 # Fetch all of new (not in index) Ethereum blocks and add transactions to index
+def get_block_with_retry(blockHeight, retries=3, delay=5):
+    for i in range(retries):
+        try:
+            return web3.eth.get_block(blockHeight, True)
+        except TimeExhausted:
+            if i < retries - 1:
+                time.sleep(delay)
+            else:
+                raise
+
 while True:
     try:
-        conn = psycopg2.connect(database=dbname)
+        logger.info("Trying to connect to " + databasename + " database…")
+        conn_string = f"dbname={databasename} user={dbuser} password={dbpassword} host={dbhost}"
+        conn = psycopg2.connect(conn_string)
         conn.autocommit = True
-    except:
-        logger.error("Unable to connect to database")
+    except Exception as e:
+        logger.error(f"Unable to connect to database: {e}")
 
     cur = conn.cursor()
 
     cur.execute('SELECT Max(block) from public.ethtxs')
     maxblockindb = cur.fetchone()[0]
-    # On first start, we index transactions from a block number you indicate
     if maxblockindb is None:
         maxblockindb = int(startBlock)
 
@@ -146,7 +161,7 @@ while True:
     logger.info('Current best block in index: ' + str(maxblockindb) + '; in Ethereum chain: ' + str(endblock))
 
     for blockHeight in range(maxblockindb + 1, endblock):
-        block = web3.eth.get_block(blockHeight, True)
+        block = get_block_with_retry(blockHeight)
         if len(block.transactions) > 0:
             insertTxsFromBlock(block)
             logger.info('Block ' + str(blockHeight) + ' with ' + str(len(block.transactions)) + ' transactions is processed')
